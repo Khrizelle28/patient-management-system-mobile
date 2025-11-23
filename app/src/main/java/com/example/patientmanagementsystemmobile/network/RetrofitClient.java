f15d9ebbe7cc0b5d6ee48fc43f1e4d6448db8014
Khrizelle28 <sablankhrizelle@gmail.com>
2025-11-23 16:22:58 +0800

    fixed in webhost

--- app/src/main/java/com/example/patientmanagementsystemmobile/network/RetrofitClient.java
+++ app/src/main/java/com/example/patientmanagementsystemmobile/network/RetrofitClient.java
@@ -11,8 +11,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
 
-//    private static final String BASE_URL = "http://10.0.2.2:8000/api/";
-    private static final String BASE_URL = "https://patient-management-system-main-2zywvz.laravel.cloud/api/";
+    private static final String BASE_URL = "http://10.0.2.2:8000/api/";
+//    private static final String BASE_URL = "https://patient-management-system-main-2zywvz.laravel.cloud/api/";
     private static Retrofit retrofit;
     private static Context appContext;
 
